9c7796552e209686c4a2d7db051943b4581352ac
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-03-27 13:13:55 +0530

    Update Program11.rb

--- Program11.rb
+++ Program11.rb
@@ -1,1 +1,8 @@
 #Write a Ruby program to truncate a given string to the first n words.
+#Write a Ruby program to remove a character from a given string if it starts with that specified character
+string = "I am Jyoti.
+I am from Noida."
+j = "I"
+if j.length < string.length 
+    puts string.delete('.')
+end
