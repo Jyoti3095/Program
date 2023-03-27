a1ba949b213a109c571caae3dd264e063c12f848
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-03-27 10:27:43 +0530

    Create Program4.rb

---
+++ Program4.rb
@@ -0,0 +1,6 @@
+#Write a Ruby program to remove last specified characters from a given string
+name = "Welcome"
+ puts name.delete!('e')
+ puts name.delete!('m')
+ puts name.delete!('o')
+ puts name.delete!('c')
