558cf8cac39121875988823012d14876a2ec2fd8
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-03-27 12:03:40 +0530

    Create Program9.rb

---
+++ Program9.rb
@@ -0,0 +1,9 @@
+#Write a Ruby program to check whether a string starts with an other string
+string_1 = "NewRubyProgram"
+string_2 = "New"
+if string_1.length>string_2.length
+    puts "Yes string starts with an another string"
+else
+    puts "False"
+    
+end
