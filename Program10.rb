95c92d7f8d4ed0e3645d2674d5058d24974133ed
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-03-27 12:10:05 +0530

    Create Program10.rb

---
+++ Program10.rb
@@ -0,0 +1,5 @@
+#Write a Ruby program to sort a string's characters alphabetically
+#Write a Ruby program to trim specific characters from a string.
+word = "Remove a character"
+puts word.delete('a')
+puts word.delete('e')
