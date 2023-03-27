a6a433f25d48ee8a7d8141a415255dc3483778f4
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-03-27 13:01:10 +0530

    Create Program12.rb

---
+++ Program12.rb
@@ -0,0 +1,6 @@
+#Write a Ruby program to get a substring from a specified position to the last char of a given string
+#Write a Ruby program to get the number of lines in a given string
+string = "I am Jyoti.
+I am from Noida.
+I am learning Ruby"
+puts "No of lines -",string.lines.count
