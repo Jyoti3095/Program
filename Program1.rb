502f425ec85915b5dd3a701e9ba6833d48b6ad64
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-03 00:16:40 +0530

    Update Program1.rb

--- Program1.rb
+++ Program1.rb
@@ -1,4 +1,5 @@
 #Write a Ruby program to insert a string of length 2 to an another string where the first string will be in the middle of the second string
-x = "Jyo"
-y = "ti"
-puts x + y
+x = 'Jyoti'
+'Jyoti'.insert(2, 'yy')
+
+
