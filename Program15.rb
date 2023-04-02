7b2c63a6251806489d0cdf8ddc47c15939eb4a8d
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-03 00:58:15 +0530

    Create Program15.rb

---
+++ Program15.rb
@@ -0,0 +1,11 @@
+# Write a Ruby program to check whether 7 appears as either the first or last element in a given array. The array length must be 1 or more
+array = [7,6,5,4,3]
+ifhttps://www.onlinegdb.com/online_ruby_compiler#editor_1
+ array[0] = 7
+ puts 'true'
+ elsif 
+     array[4] = 7
+     puts 'true'
+ else
+     puts 'false'
+ end
