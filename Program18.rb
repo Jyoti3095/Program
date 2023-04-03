0458e19608cf8dd573834e2590227ddbcc8b100e
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-03 11:50:13 +0530

    Update Program18.rb

--- Program18.rb
+++ Program18.rb
@@ -1,3 +1,7 @@
 # Write a Ruby program to compute the sum of elements in a given array
 p = [1,2,3,4,5,6 ]
 puts p.sum
+# Write a Ruby program to remove duplicate elements from a given array
+p = [1,2,3,4,5,6,7,1,2,3]
+puts p.uniq!
+
