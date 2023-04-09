02a0a5272dfa3a48a71807d2d8c563c8b39a5f5b
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-10 02:37:33 +0530

    Create Program27.rb

---
+++ Program27.rb
@@ -0,0 +1,3 @@
+#Write a Ruby program to find the larger between the first and last elements of a given array of integers of length 3. Replace all the other values to be that value. Return the changed array. 
+
+ 
