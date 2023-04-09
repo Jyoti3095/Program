4bdd7ded7cf66fa10c62f897bb9a50393a8d5083
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-10 03:01:35 +0530

    Create Program29.rb

---
+++ Program29.rb
@@ -0,0 +1,3 @@
+#Count the occurrences of various alphabet letters in an input string and store it in
+the hash. Your ruby program should accept a string as an argument and display the hash
+as an output.
