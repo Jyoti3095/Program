fa33cd6f4d4f82a7862c3ff1069be4a1a02cbeef
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-10 02:22:37 +0530

    Create Program25.rb

---
+++ Program25.rb
@@ -0,0 +1,16 @@
+#Write a Ruby program to get the number of even integers in a given array.
+array1 = Array.new(5)
+puts array1[0] = 1
+puts array1[1] = 2
+puts array1[2] = 3
+puts array1[3] = 4
+puts array1[4] = 5
+idx = 0   
+while  idx < array1.length    
+   if
+  array1[idx] % 2 == 0 
+  puts "Even no are #{array1[idx]}" 
+  end 
+  idx +=1
+    
+en
