2b4c60015e6699660edb4fa583d9e504ee0682ae
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-10 01:42:36 +0530

    Create Program23.rb

---
+++ Program23.rb
@@ -0,0 +1,7 @@
+#Write a Ruby program to create a new array with the elements in reverse order from a given an array of integers length 3
+array = Array.new(4)
+puts array[0] = "One"
+puts array[1] = "Two"
+puts array[2] = "Three"
+puts array[3] = "Four"
+puts "#{array.reverse()}"
