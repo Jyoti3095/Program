0cde72036eb951c6bd8a95f92dba7bc71437abf4
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-10 12:26:25 +0530

    Update Program30.rb

--- Program30.rb
+++ Program30.rb
@@ -1,3 +1,7 @@
 #Ask the user to enter text. Replace each vowel in the text with a '*' using a regular
 expression. Your program should accept a string as an argument and output the
 replaced string
+input = gets
+input = input.gsub!(/[aeiou]/, "*")
+puts input
+       
