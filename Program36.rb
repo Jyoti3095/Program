a6416b70c9ce3072a3d5ffeeba4361090943e0c4
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-21 02:28:21 +0530

    Create Program36.rb

---
+++ Program36.rb
@@ -0,0 +1,12 @@
+# Write a function to reverse an array.
+array = [22,44,56,77,87,98]
+def reverse(array)
+    puts array.reverse   
+end     
+reverse(array)
+# Create a function that takes an array of numbers and returns the smallest number in the array.
+array = [22,44,56,77,87,98]
+def smallest(array)
+   puts array.min
+end            
+smallest(array)
