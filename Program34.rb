2eed2252272c1f4913c32641e2c27493e15fe184
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-21 01:55:09 +0530

    Create Program34.rb

---
+++ Program34.rb
@@ -0,0 +1,6 @@
+#Create a function that accepts an array and returns the last item in the array. The array can be either homogeneous or heterogeneous.
+array = [1, 2, 3]
+def last_index(array)
+    puts array.last
+end         
+last_index(array)
