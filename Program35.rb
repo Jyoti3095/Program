cf5054a8fc4a5ff795e305b279b373d889576166
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-21 02:12:51 +0530

    Create Program35.rb

---
+++ Program35.rb
@@ -0,0 +1,11 @@
+# Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+def compare(num)
+    if    
+        num <= 0       
+        puts "true"
+    else
+        puts "false"
+        
+    end
+end     
+compare(9)
