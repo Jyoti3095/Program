b4264458634b6087cb5bb10f45e1478559925846
Jyoti3095 <123929341+Jyoti3095@users.noreply.github.com>
2023-04-25 17:20:35 +0530

    Update Program39.rb

--- Program39.rb
+++ Program39.rb
@@ -1,1 +1,16 @@
 #Create a function which returns the type of triangle, given the side lengths. Return the following values if they match the criteria.
+def triangle(a, b, c)
+    if 
+        a == b && b == c    
+        puts "this is an equilateral triangle"
+        
+    elsif 
+        a == b && a != c
+            puts "This is an isosceles triangle"
+        else 
+            a != b && b != c      
+            puts "This is an isosceles triangle"
+        
+    end
+end
+    triangle(4, 5, 6)
